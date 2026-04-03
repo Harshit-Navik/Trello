84709551cdf305d81c21cab7b4515aeb635b7b04
Harshit-Navik <hrxit1984@gmail.com>
2026-04-04 00:05:21 +0530

    initialize frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+    <div
+    className='bg-black text-white' 
+    > initiating trello - frontend will be created after completing react </div>
+    </>
+  )
+}
+
+export default App
