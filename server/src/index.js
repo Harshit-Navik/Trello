25a50e2a7728f66da6f207b55e578dbe9c690b78
Harshit-Navik <hrxit1984@gmail.com>
2026-04-05 22:44:43 +0530

    add server entry point

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,17 @@
 // this is the entry point of our server , server will starts from here 
-
+import express from "express";
 import  connectDB  from "./db/index.js";
+const app = express();
 
 connectDB()
+.then(() => {
+    app.listen(`${process.env.PORT || 8000}`, () => {
+        `server is listening on port: ${process.env.PORT}`
+    })
+})
+.catch((err) => {
+    console.log(`Error connecting server`);
+    throw err;
+})
 
 
