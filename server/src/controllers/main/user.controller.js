6786754939dbb35035489489ad2a24ae72756e83
Harshit-Navik <hrxit1984@gmail.com>
2026-04-15 23:19:58 +0530

    add register user controller

--- server/src/controllers/main/user.controller.js
+++ server/src/controllers/main/user.controller.js
@@ -1,13 +1,64 @@
+import { ApiError } from "../../utils/ApiError";
 import { asyncHandler } from "../../utils/asyncHandler";
+import User from "../../models/user.model.js"
+import { ApiResponse } from "../../utils/ApiResponse.js";
+
+
 
 const registerUser = asyncHandler(async (req, res) => {
-    
+
     // ----- steps to perform to register a user -----
 
     /*
         1. get user details from frontend 
         2. validation - non empty 
+        3. check if user already exists : username / password 
+        4. check for images or avatars and upload them to cloudinary { optional }
+        5. remove password and refresh token field from response 
+        6. check for user creation 
+        7. return response
     */
+
+
+    const { fullname, username, email, password } = req.body;
+
+    if (
+        [fullname, username, email, password].some(field => field?.trim() === "") // if any single element is empty then it will throw an error
+    ) {
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const existingUser = User.findOne({
+        $or: [{username} , {email}]
+    })
+
+    if(existingUser){
+        throw new ApiError(409 , "User with this email or username already exists")
+    }
+
+    // here we creates the user object to store in db ...
+
+    const user = await User.create({
+        fullname, 
+        username : username.toLowerCase(), 
+        password,
+        email
+    })
+
+    // here we checks if the user object is created or not , if its created then remove password and refreshtoken field by using select method { its syntax is kind of wierd as it accepts a string in which we have to pass the fields we dont wants with "-" symbol , as all are selected by default } 
+
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    )
+
+    if(!createdUser){
+        throw new ApiError(500 , "  something went wrong while registering the user")
+    }
+
+    return res.status(201).json(
+        new ApiResponse(200 , createdUser , "user registered successfully ")
+    )
+
 })
 
 export { registerUser }
--- server/src/models/user.model.js
+++ server/src/models/user.model.js
@@ -1,28 +1,83 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import { use } from "react";
 
-const userSchema = new mongoose.Schema({ 
 
-    userName: {
-        type: String, 
+const userSchema = new Schema({
+    username: {
+        type: String,
         required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true    // used to enable searching , optimized way 
     },
-
     email: {
         type: String,
         required: true,
-        lowercase: true,
         unique: true,
-        trim: true
+        lowercase: true,
+        trim: true,
     },
-
-    password: {
+    fullname: {
         type: String,
-        required: [true , "password is required"],
-        minlength: [6, "password must have atleast 6 letters"],
-        maxlength: 12,
+        required: true,
+        trim: true,
+        index: true    // used to enable searching , optimized way 
+    },
+    // avatar: {
+    //     type: String, // cloudinary url 
+    //     required: true,
+    // },
+    // coverImage: {
+    //     type: String
+    // },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    password: {
+        type: String, // string because its going to be encrypted 
+        required: [true, "Password is required"],
+    },
+    refreshToken: {
+        type: String
     }
- 
-} , { timestamps : true })
+}, { timestamps: true })
 
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
 
-export const User = mongoose.model("User" , userSchema);
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+}) // first parameter is a functionallity that u wanna perform like save , delete etc , second one is callback , always use normal function here as arrow function doesnt have context of this keyword , that was needed the most 
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+    },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        })
+
+};
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
+        _id: this._id,
+    },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        })
+};
+
+export const User = mongoose.model("User", userSchema); 
