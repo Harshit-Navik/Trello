48a9d37106f38cedcc272837e7b04e8765e05ffc
Harshit-Navik <hrxit1984@gmail.com>
2026-04-29 16:44:17 +0530

    add create org controller

--- server/src/routes/organizations.route.js
+++ server/src/routes/organizations.route.js
@@ -1,27 +1,30 @@
 import { Router } from "express";
-import boardRouter from "./boards.route.js"
+// import boardRouter from "./boards.route.js"
+// import { createOrg } from "../schema/org.schema.js";
+import { createOrganization } from "../controllers/organization.controller.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router
-    .route("/")
-    .get() // get all org 
-    .post() // create an org 
+    .route("/").post(authMiddleware ,createOrganization)
+    // .get() // get all org 
+    // .post() // create an org 
 
-router
-    .route("/:orgId")
-    .get() // get specific org 
-    .patch() // update org
-    .delete() // delete org 
+// router
+//     .route("/:orgId")
+//     .get() // get specific org 
+//     .patch() // update org
+//     .delete() // delete org 
 
-router
-    .route("/:orgId/members")
-    .post() // add member
+// router
+//     .route("/:orgId/members")
+//     .post() // add member
 
-router
-    .route("/:orgId/members/:membersId")
-    .delete() // remove member 
+// router
+//     .route("/:orgId/members/:membersId")
+//     .delete() // remove member 
 
-router.use("/:orgId/boards" , boardRouter)
+// router.use("/:orgId/boards" , boardRouter)
 
 export default router;
