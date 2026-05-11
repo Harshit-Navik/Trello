1808bdb78c21eab1f71acfdb989e646d82088b9d
Harshit-Navik <hrxit1984@gmail.com>
2026-05-11 06:18:23 +0530

    update middleware

--- server/src/routes/boards.route.js
+++ server/src/routes/boards.route.js
@@ -3,23 +3,23 @@ import { Router } from "express";
 // import listRouter from "./lists.route.js";
 
 import { board } from "../controllers/board.controller.js";
-import authMiddleware from "../middlewares/auth.middleware.js";
+import verifyJWT from "../middlewares/auth.middleware.js";
 
 const router = Router({ mergeParams: true });
 
+router.use(verifyJWT)
+
 router
-    .route("/").post(authMiddleware, board.create) // to create board 
-    .get(authMiddleware, board.getAll) // to get all boards
+    .route("/")
+    .post(board.create) // to create board 
+    .get(board.getAll) // to get all boards
 
 
 router
     .route("/:boardId")
-    .patch(authMiddleware, board.update) // update board
+    .patch(board.update) // update board
 //     .get() // get specific board
 //     .delete() // delete board
 
-router
-    .route("/:boardId/lists")
-    .get(listRouter);
 
 export default router;
--- server/src/routes/organizations.route.js
+++ server/src/routes/organizations.route.js
@@ -1,28 +1,30 @@
 import { Router } from "express";
 import boardRouter from "./boards.route.js"
 import { organization } from "../controllers/organization.controller.js";
-import authMiddleware from "../middlewares/auth.middleware.js";
+import verifyJWT from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
+router.use(verifyJWT)
+
 router
-    .route("/").post(authMiddleware, organization.create)
-    .get(authMiddleware, organization.getAll) // get all org 
+    .route("/").post(organization.create)
+    .get(organization.getAll) // get all org 
 
 router
     .route("/:orgId")
-    .get(authMiddleware, organization.getParticular) // get specific org 
-    .patch(authMiddleware, organization.updateTitle) // update org   
+    .get(organization.getParticular) // get specific org 
+    .patch(organization.updateTitle) // update org   
 //     .delete() // delete org 
 
 router
     .route("/:orgId/members")
-    .post(authMiddleware, organization.addMember) // add member
+    .post(organization.addMember) // add member
 
 router
     .route("/:orgId/members/:memberId")
-    .delete(authMiddleware, organization.removeMember) // remove member 
+    .delete(organization.removeMember) // remove member 
 
-router.use("/:orgId/boards" , boardRouter)
+router.use("/:orgId/boards", boardRouter)
 
 export default router;
